59bbc12599410aa3192f07d1c4fa3b3341fd8339
Samandar2001 <samandaribrohimov2001@gmail.com>
2024-01-13 15:50:57 +0000

    Update config.py

--- data/config.py
+++ data/config.py
@@ -6,7 +6,7 @@ env.read_env()
 # BOT_TOKEN = '5204190868:AAGdR_cSlHjxw8S1nkkSUyoCr3lTMivjgMw'
 # ADMINS = ['1024522810']
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "5436622255:AAHj6lp2u_xmGq8bVNY4H-FRlgKNvFy6BGA"#env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = "5268428809:AAG-n1suE_60J5sJjK6gEPmuirpetxkSVFk"#env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = [1024522810,2090089858]#env.list("ADMINS")  # adminlar ro'yxati
 IP = 'localhost'#env.str("ip")  # Xosting ip manzili
 TELEGRAM_SUPPORT_CHAT_ID = -1001772344700#env.int('TELEGRAM_SUPPORT_CHAT_ID')
